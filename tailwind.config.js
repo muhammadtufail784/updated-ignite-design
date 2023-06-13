757a9cf5fd3089146e554034345130f46f6189d2
muhammadtufail784 <mr.tufailkhangamer@gmail.com>
2023-06-13 18:15:20 +0500

    new updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        public: ['Public Sans', "sans-serif"],
-        inter: ['Inter', "sans-serif"],
-        nunito: ['Nunito Sans', "sans-serif"],     
-        Lato: ['Lato', "sans-serif"]
+        // public: ['Public Sans', "sans-serif"],
+        // inter: ['Inter', "sans-serif"],
+        // nunito: ['Nunito Sans', "sans-serif"],     
+        // Lato: ['Lato', "sans-serif"],
+
+
+     
+          sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+          Inter: ["Inter", "sans-serif"],
+          Nunito: ["Nunito Sans", "sans-serif"],
+          Lato: ["Lato", "sans-serif"],
+          Roboto: ["Roboto", "sans-serif"],
+          Poppins: ["Roboto", "sans-serif"],
+          Public: ["Public Sans", "sans-serif"],
+          DM:['DM Sans', 'sans-serif']
+   
+
+
 
       }
     },
